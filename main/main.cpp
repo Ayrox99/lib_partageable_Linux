a517da0623ec081e37f31939ab17bf08d7c7bcc1
Ayrox99 <56259172+Ayrox99@users.noreply.github.com>
2021-03-08 00:20:05 +0100

    Version Finale
    Code cleané, etc

--- main/main.cpp
+++ main/main.cpp
@@ -8,11 +8,10 @@ int main(int argc, char* argv[])
 {
     void* handle1;
     void* handle2;
-    //double (*composant1)(double);
-    //double (*composant2)(double);
     int (*composant1)(int, int);
     int (*composant2)(int, int);
     char* error;
+    char* (*getComposant1Version)();
 
     handle1 = dlopen(argv[1], RTLD_LAZY);
     if (!handle1) {
@@ -28,13 +27,11 @@ int main(int argc, char* argv[])
 
     dlerror();    /* Clear any existing error */
 
-    //*(void**)(&composant1) = dlsym(handle1, "_Z10composant1ii");
     composant1 = (int (*)(int, int)) dlsym(handle1, "_Z10composant1ii");
     if ((error = dlerror()) != NULL) {
         fprintf(stderr, "%s\n", error);
         exit(EXIT_FAILURE);
     }
-    //*(void**)(&composant2) = dlsym(handle2, "_Z10composant2ii");
     composant2 = (int (*)(int, int)) dlsym(handle2, "_Z10composant2ii");
     if ((error = dlerror()) != NULL) {
         fprintf(stderr, "%s\n", error);
@@ -48,12 +45,15 @@ int main(int argc, char* argv[])
 	int valeur2;
 	
 	valeur1=composant1(data1,data2);
-
 	valeur2=composant2(data1,data2);
+    cout << "valeur 1 :" << valeur1 << " valeur 2 :" << valeur2;
 
-	//std::cout << getComposant1Version() << std::endl;
-	std::cout << "valeur 1 :" << valeur1 << " valeur 2 :" << valeur2 << std::endl;
-	//std::cout << *argv[3] << std::endl;
+    getComposant1Version = (char* (*)()) dlsym(handle1, "_Z20getComposant1Versionv");
+    if ((error = dlerror()) != NULL) {
+        fputs(error, stderr);
+        exit(1);
+    }
+    cout << getComposant1Version();
 
     dlclose(handle1);
     dlclose(handle2);
